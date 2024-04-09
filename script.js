ea3bf1dd80399455fb9c69115c6de9e26db7988c
CryCatCC <160016979+CryCatCC@users.noreply.github.com>
2024-04-10 00:10:05 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+// const difficultySelect = document.getElementById('difficulty');
+// const colorSelect = document.getElementById('color');
+// const startButton = document.getElementById('start-game');
+
+// startButton.addEventListener('click', function() {
+//   const difficulty = difficultySelect.value;
+//   const color = colorSelect.value;
+  
+//   // Prevent page reload
+//   event.preventDefault();
+  
+//   // Pass data to the new game page using URL parameters
+//   window.location.href = `index.html`
