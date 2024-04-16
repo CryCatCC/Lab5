c5e63d8c0f3f6f910a8451f12222e4c42db22b62
CryCatCC <160016979+CryCatCC@users.noreply.github.com>
2024-04-16 19:23:09 +0300

    Update script.js

--- script.js
+++ script.js
@@ -6,8 +6,7 @@
 //   const difficulty = difficultySelect.value;
 //   const color = colorSelect.value;
   
-//   // Prevent page reload
+
 //   event.preventDefault();
   
-//   // Pass data to the new game page using URL parameters
 //   window.location.href = `index.html`
