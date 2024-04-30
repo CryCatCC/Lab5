6c82d58b4bacb006a9854ca43918192e95c1d8d8
CryCatCC <160016979+CryCatCC@users.noreply.github.com>
2024-04-30 21:47:00 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,75 @@
-// const difficultySelect = document.getElementById('difficulty');
-// const colorSelect = document.getElementById('color');
-// const startButton = document.getElementById('start-game');
+let square = document.querySelector("#square")
+let color = document.querySelector("#color")
+let difficulty = document.querySelector("#difficulty")
+let gameStartButton = document.querySelector("#gameStart")
+let form1 = document.querySelector("#form1")
+let time = 0;
+let timeRefresh = 0;
+let score = document.querySelector("#score")
+let scoreInteger = 0;
+let clickTimeLeft = document.querySelector("#clickTimeLeft")
 
-// startButton.addEventListener('click', function() {
-//   const difficulty = difficultySelect.value;
-//   const color = colorSelect.value;
-  
+function DecreaseTime()
+{
+    time--
+     
+    if (time == -1) {
+        
+      alert("Time's out! You've lost")
+      location.reload()
+    }
+    clickTimeLeft.innerHTML = "Time for click left: "+ time
+    score.innerHTML = "Score: "+ scoreInteger
+}
 
-//   event.preventDefault();
-  
-//   window.location.href = `index.html`
+square.addEventListener("click", function()
+{
+    time=timeRefresh
+    square.style.left = getRandomArbitrary(100,600)+"px"
+    square.style.top = getRandomArbitrary(100,600)+"px"
+    scoreInteger++
+})
+
+gameStartButton.addEventListener("click",function(event)
+{
+   
+if (color.options[color.selectedIndex].value == "--Choose select--" || difficulty.options[difficulty.selectedIndex].value
+== "--Choose select--" )
+{
+    alert("Choose not a "+'`Choose select`'+" option!")
+    location.reload()
+}
+        setInterval(DecreaseTime,1000)
+        event.preventDefault()
+        form1.style.display = "none";
+        let diff = difficulty.options[difficulty.selectedIndex].value
+
+    if(diff == "Easy")
+    {
+        timeRefresh = 5
+        time=timeRefresh
+        square.style.width = "50px"
+        square.style.height = "50px"
+    }
+    else if (diff == "Normal")
+    {
+        timeRefresh = 3
+        time=timeRefresh
+        square.style.width = "30px"
+        square.style.height = "30px"
+    }
+    else 
+    {
+        timeRefresh = 2
+        time=timeRefresh
+        square.style.width = "20px"
+        square.style.height = "20px"
+    }
+
+    square.style.background = color.options[color.selectedIndex].value //поява
+    form2.style.display = "block";
+})
+
+function getRandomArbitrary(min, max) {
+    return Math.random() * (max - min) + min;
+  }
